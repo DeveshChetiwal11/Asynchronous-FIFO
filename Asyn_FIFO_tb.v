99b7344f955ac0f2d4c28c0a76b11a34973a769e
Devesh Chetiwal <devesh.chetiwal.21042@iitgoa.ac.in>
2025-05-31 18:40:02 +0530

    Update Asyn_FIFO_tb.v

--- Asyn_FIFO_tb.v
+++ Asyn_FIFO_tb.v
@@ -9,7 +9,7 @@ reg [DATA_WIDTH-1:0] wr_data;
 wire [DATA_WIDTH-1:0] rd_data;
 wire full,empty;
 
-Async_FIFO dut (.wr_clk(wr_clk),.rst(rst),.rd_clk(rd_clk),.wr_en(wr_en),.rd_en(rd_en),.rd_data(rd_data),.wr_data(wr_data),.full(full).empty(empty));
+	Async_FIFO dut (.wr_clk(wr_clk),.rst(rst),.rd_clk(rd_clk),.wr_en(wr_en),.rd_en(rd_en),.rd_data(rd_data),.wr_data(wr_data),.full(full),.empty(empty));
 
 initial begin
 $monitor("time = %0t, wr_en = %b,rd_en = %b,wr_data = %h,rd_data =%h, full = %b,empty =%b",$time,wr_en,rd_en,wr_data,rd_data,full,empty);
